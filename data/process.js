907e85ebfead26d3bc07469d1e2f2c6d8e133eb5
visualer <api03025@gmail.com>
2018-07-08 03:43:42 +0800

    Update 2018-07-07T19:43:37.160Z

--- data/process.js
+++
@@ -1,6 +0,0 @@
-let fs = require('fs');
-let poster = JSON.parse(fs.readFileSync('oral.json', {encoding: 'utf8'}));
-
-console.log(poster.length);
-
-// fs.writeFileSync('abstracts_processed.json', JSON.stringify(poster, null, 2), { encoding: 'utf8' });
